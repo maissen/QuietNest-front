410c3576bc6553ba34277aa8429f73661e1647e2
Maissen Belgacem <maissen.developer500@gmail.com>
2025-01-03 13:02:50 +0100

    Initialize 'audio-item-rectangle' component.

--- src/app/shared/audio-item-rectangle/audio-item-rectangle.component.ts
+++ src/app/shared/audio-item-rectangle/audio-item-rectangle.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./audio-item-rectangle.component.scss']
 })
 export class AudioItemRectangleComponent {
+  img_src: string = 'https://images.squarespace-cdn.com/content/v1/607f89e638219e13eee71b1e/1684821560422-SD5V37BAG28BURTLIXUQ/michael-sum-LEpfefQf4rU-unsplash.jpg';
+  img_alt: string = 'Cat';
 
+  audio_title: string = "Stress and burnout support";
+  audio_categories: string[] = ['calm', 'meditation'];
+  audio_duration: number = 32;
+
+  author: string = 'Matthew Brenher';
+  
+  isNew: boolean = true;
 }
