0b64fc9cba9a44fdab045c518bf14ef2676de9a8
Maissen Belgacem <maissen.developer500@gmail.com>
2025-01-05 13:46:24 +0100

    Create 'categories_header' in 'ceate-section' component.

--- src/app/pages/create-section/create-section.component.ts
+++ src/app/pages/create-section/create-section.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-create-section',
   templateUrl: './create-section.component.html',
   styleUrls: ['./create-section.component.scss']
 })
-export class CreateSectionComponent {
+export class CreateSectionComponent implements AfterViewInit {
+  @ViewChild('categoriesList') categoriesList!: ElementRef<HTMLUListElement>;
 
+  canScrollLeft = false;
+  canScrollRight = true;
+
+  ngAfterViewInit() {
+    this.updateScrollButtons();
+    this.categoriesList.nativeElement.addEventListener('scroll', () => this.updateScrollButtons());
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.updateScrollButtons();
+  }
+
+  scrollLeft() {
+    this.categoriesList.nativeElement.scrollBy({
+      left: -200,
+      behavior: 'smooth',
+    });
+    this.updateScrollButtons();
+  }
+
+  scrollRight() {
+    this.categoriesList.nativeElement.scrollBy({
+      left: 200,
+      behavior: 'smooth',
+    });
+    this.updateScrollButtons();
+  }
+
+  updateScrollButtons() {
+    const element = this.categoriesList.nativeElement;
+    this.canScrollLeft = element.scrollLeft > 0;
+    this.canScrollRight = element.scrollLeft + element.clientWidth < element.scrollWidth;
+  }
 }
