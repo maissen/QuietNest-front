f7ee14a68f4260913f80ee294be290861708add9
Maissen Belgacem <maissen.developer500@gmail.com>
2025-01-06 01:03:10 +0100

    Create 'audio service' to fetch sample audios data from the expressJS server and use it within the 'browse section' component.

--- src/app/pages/browse-section/browse-section.component.ts
+++ src/app/pages/browse-section/browse-section.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AudioService } from 'src/app/services/audio.service';
 
 @Component({
   selector: 'app-browse-section',
   templateUrl: './browse-section.component.html',
   styleUrls: ['./browse-section.component.scss']
 })
-export class BrowseSectionComponent {
+export class BrowseSectionComponent implements OnInit{
+
+  constructor(private audioService: AudioService){};
+
+  ngOnInit(): void {
+      this.audioService.getAudioData().subscribe(
+        (data) => {
+          console.log(data);
+        }
+      )
+  }
 
 }
