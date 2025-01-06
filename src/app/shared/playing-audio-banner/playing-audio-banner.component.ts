341019200fc14bf620f9630d22ec83b6b8ac491e
Maissen Belgacem <maissen.developer500@gmail.com>
2025-01-06 02:07:38 +0100

    Update 'playing-audio-banner' component when an audio-item is clicked.

--- src/app/shared/playing-audio-banner/playing-audio-banner.component.ts
+++ src/app/shared/playing-audio-banner/playing-audio-banner.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PlayingAudioService } from 'src/app/services/playing-audio.service';
 
 @Component({
   selector: 'app-playing-audio-banner',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./playing-audio-banner.component.scss']
 })
 export class PlayingAudioBannerComponent {
+  selectedAudioData: any = {};  // This will store the audio data directly
 
+  constructor(private playingAudioService: PlayingAudioService) {}
+
+  ngOnInit() {
+    // Subscribe to the audio data and update the component when the data changes
+    this.playingAudioService.selectedAudioData$.subscribe(audioData => {
+      this.selectedAudioData = audioData;
+      if (this.selectedAudioData) {
+        // Do something with the data, for example, update the component's view
+        // console.log('Currently selected audio:', this.selectedAudioData);
+      }
+    });
+  }
 }
