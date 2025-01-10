7a6e0cb779b7686881be9e5dacc2bf2982c32c36
Maissen Belgacem <maissen.developer500@gmail.com>
2025-01-10 15:10:42 +0100

    Update 'bg-video' component.

--- src/app/shared/bg-video/bg-video.component.ts
+++ src/app/shared/bg-video/bg-video.component.ts
@@ -1,30 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { ScenesSectionService } from 'src/app/services/scenes-section.service';
+import { ScenesService } from 'src/app/services/scenes.service';
 
 @Component({
   selector: 'app-bg-video',
   templateUrl: './bg-video.component.html',
   styleUrls: ['./bg-video.component.scss']
 })
-export class BgVideoComponent implements OnInit {
-  
-  currentScene: any;
+export class BgVideoComponent {
 
-  constructor(private scenesServices: ScenesSectionService) {};
+  constructor(private scenesService: ScenesService) {
 
-  ngOnInit(): void {
+  };
 
-    this.scenesServices.getCurrentScene().subscribe(
-      (scene) => {
-        this.currentScene = scene;
-        if (this.currentScene) {
-          // console.log('create comp, Current scene:', this.currentScene.name);
-        }
-      },
-      (error) => {
-        console.error('Error fetching current scene:', error);
-      }
-    );
-
+  getActiveScene(): any {
+    return this.scenesService.getActiveScene();
   }
+
 }
