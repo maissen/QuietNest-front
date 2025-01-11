0d0ee7d87587f57e7425e0a336e34ab0b29b8b3b
Maissen Belgacem <maissen.developer500@gmail.com>
2025-01-11 23:53:12 +0100

    Initialize 'speech-section' component.

--- src/app/pages/speech-section/speech-section.component.ts
+++ src/app/pages/speech-section/speech-section.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PlayingSpeechService } from 'src/app/services/playing-speech.service';
 
 @Component({
   selector: 'app-speech-section',
   templateUrl: './speech-section.component.html',
   styleUrls: ['./speech-section.component.scss']
 })
-export class SpeechSectionComponent {
+export class SpeechSectionComponent implements OnInit {
 
+  constructor(
+    public service: PlayingSpeechService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    if (!this.service.getSelectedSpeechData()) {
+      this.router.navigate(['/app/browse']);
+    }
+  }
 }
