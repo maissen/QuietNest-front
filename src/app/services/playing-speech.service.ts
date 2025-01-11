7f5bb486b672b37a2652cbafe0f2e0f62e809833
Maissen Belgacem <maissen.developer500@gmail.com>
2025-01-11 21:11:08 +0100

    Hide 'playing-speech' component when opening 'speech-section' or 'playlist-section' component.

--- src/app/services/playing-speech.service.ts
+++ src/app/services/playing-speech.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { SpeechesService } from './speeches.service';
 import { CategoriesService } from './categories.service';
 import { NarratorsService } from './narrators.service';
 
--- src/app/shared/playing-speech-banner/playing-speech-banner.component.ts
+++ src/app/shared/playing-speech-banner/playing-speech-banner.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { CreateSectionService } from 'src/app/services/create-section.service';
 import { PlayingSpeechService } from 'src/app/services/playing-speech.service';
 
@@ -8,10 +9,21 @@ import { PlayingSpeechService } from 'src/app/services/playing-speech.service';
   styleUrls: ['./playing-speech-banner.component.scss']
 })
 export class PlayingSpeechBannerComponent {
+  hideBanner: boolean = false;
 
   constructor(
     public service: PlayingSpeechService,
-    public createSectionService: CreateSectionService
-  ) {}
+    public createSectionService: CreateSectionService,
+    private router: Router
+  ) {
+    this.router.events.subscribe(() => {
+      this.checkRoute();
+    });
+    this.checkRoute();
+  }
 
+  private checkRoute(): void {
+    const url = this.router.url;
+    this.hideBanner = url.startsWith('/app/speech/') || url.startsWith('/app/playlist/');
+  }
 }
