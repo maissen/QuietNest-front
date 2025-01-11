e0a06abea920edee1a304d880ee0e874dcc45742
Maissen Belgacem <maissen.developer500@gmail.com>
2025-01-11 11:54:44 +0100

    Connect 'recommended-for-you-item' component with 'playing-speech-banner' one.

--- src/app/shared/recommended-for-you-item/recommended-for-you-item.component.ts
+++ src/app/shared/recommended-for-you-item/recommended-for-you-item.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { NarratorsService } from 'src/app/services/narrators.service';
+import { PlayingSpeechService } from 'src/app/services/playing-speech.service';
+import { SpeechesService } from 'src/app/services/speeches.service';
 
 @Component({
   selector: 'app-recommended-for-you-item',
@@ -6,15 +10,18 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./recommended-for-you-item.component.scss']
 })
 export class RecommendedForYouItemComponent {
-  @Input() data: {
-    imgSrc: string;
-    name: string;
-    likes: number;
-    categories: string[];
-  }  = {
-    imgSrc: 'https://st3.depositphotos.com/18630962/31856/i/450/depositphotos_318568256-stock-photo-huai-mae-kamin-waterfall-srinakarin.jpg',
-    name: 'Amazing Water Fall in fall season',
-    likes: 120,
-    categories: ['Nature', 'Photography', 'Travel']
-  };
+  @Input() data: any;
+
+  constructor(
+    public service: SpeechesService,
+    public categoriesService: CategoriesService,
+    public narratorsService: NarratorsService,
+    public playingSpeechService: PlayingSpeechService
+  ) { }
+
+  displaySpeechBanner() {
+    this.playingSpeechService.setSelectedSpeechData(this.data);
+    this.playingSpeechService.isPlaying();
+  }
+
 };
