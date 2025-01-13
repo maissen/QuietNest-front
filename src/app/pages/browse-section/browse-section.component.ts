fe8e48976fdaa5a040eb95ee032ce6c196f1ea29
Maissen Belgacem <maissen.developer500@gmail.com>
2025-01-13 22:58:54 +0100

    Add scrolling animation in 'browse-section' component.

--- src/app/pages/browse-section/browse-section.component.ts
+++ src/app/pages/browse-section/browse-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { BrowseSectionService } from 'src/app/services/browse-section.service';
 import { ScenesService } from 'src/app/services/scenes.service';
 
@@ -7,34 +7,21 @@ import { ScenesService } from 'src/app/services/scenes.service';
   templateUrl: './browse-section.component.html',
   styleUrls: ['./browse-section.component.scss']
 })
-export class BrowseSectionComponent implements OnInit {
-
+export class BrowseSectionComponent {
   showBgVideo: boolean = true;
+  scaleFactor: number = 1;
 
   constructor(
     public service: BrowseSectionService,
     public scenesService: ScenesService
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-    this.updateLayout();
-  }
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    const scrollPosition = window.scrollY;
+    const maxScroll = 300;
+    const scaleIncrease = 0.5;
 
-  @HostListener('window:resize', [])
-  onResize(): void {
-    this.updateLayout();
-  }
-
-  private getScreenWidth(): number {
-    return window.innerWidth;
-  }
-
-  private updateLayout(): void {
-    const screenWidth = this.getScreenWidth();
-    if (screenWidth >= 500) {
-      this.showBgVideo = false;
-    } else {
-      this.showBgVideo = true;
-    }
+    this.scaleFactor = 1 + (Math.min(scrollPosition, maxScroll) / maxScroll) * scaleIncrease;
   }
 }
