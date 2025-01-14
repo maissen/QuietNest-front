23b08e5237775c255a509642d39536f8c81aa6fd
Maissen Belgacem <maissen.developer500@gmail.com>
2025-01-14 14:48:12 +0100

    Design 'playlist-section' page.

--- src/app/pages/playlist-section/playlist-section.component.ts
+++ src/app/pages/playlist-section/playlist-section.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PlayingSpeechService } from 'src/app/services/playing-speech.service';
 
 @Component({
   selector: 'app-playlist-section',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./playlist-section.component.scss']
 })
 export class PlaylistSectionComponent {
+
+  constructor(
+    public service: PlayingSpeechService
+  ) {}
 
 }
