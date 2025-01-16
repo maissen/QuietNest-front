46ade57c9a99669a68251b6ef67a57fe4b3bf42b
Maissen Belgacem <maissen.developer500@gmail.com>
2025-01-16 15:24:37 +0100

    Ensure that data is loaded in 'app component' in the right order.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,7 +36,6 @@ export class AppComponent implements OnInit {
   
     this.checkIfUrlEndsWithApp();
     
-  
     this.router.events.subscribe(() => {
       this.checkIfUrlEndsWithApp();
     });
@@ -52,43 +51,40 @@ export class AppComponent implements OnInit {
         console.log('scene to set : ' + scene.id)
         if (scene) {
           this.scenesService.setActiveScene(scene);
+
+          //! Fetch al narrators
+          this.http.get<any[]>(this.narratorsService.api_get_all_narrators).subscribe(categories => {
+            this.narratorsService.setAllnarrators(categories);
+
+            //! Fetch all categories
+            this.http.get<any[]>(this.categoriesService.api_get_all_categories).subscribe(categories => {
+              this.categoriesService.setAllCategories(categories);
+
+              //! Fetch all speeches 
+              this.http.get<any[]>(this.speechesService.api_get_all_speeches).subscribe(speeches => {
+                this.speechesService.setSpeeches(speeches);
+
+                //! Fetch al playlists
+                this.http.get<any[]>(this.playListsService.api_all_playlists).subscribe(allPlaylists => {
+                  this.playListsService.setPlayLists(allPlaylists);
+
+                  //! Fetch all sounds and their categories
+                  this.http.get<any[]>(this.soundsService.apiGetAllSounds()).subscribe(sounds => {
+                    this.soundsService.setSounds(sounds);
+                    this.http.get<any[]>(this.soundsService.apiGetSoundsCategories()).subscribe(categories => {
+                      this.soundsService.setCategories(categories);
+                    });
+                  });
+                });
+              });
+            });
+          });
         } 
         else {
           console.warn(`No scene found with ID: ${userSceneID}`);
         }
       }
     });
-
-    //! Fetch all categories
-    this.http.get<any[]>(this.categoriesService.api_get_all_categories).subscribe(categories => {
-      this.categoriesService.setAllCategories(categories);
-    });
-
-    //! Fetch al narrators
-    this.http.get<any[]>(this.narratorsService.api_get_all_narrators).subscribe(categories => {
-      this.narratorsService.setAllnarrators(categories);
-    });
-
-    //! Fetch al playlists
-    this.http.get<any[]>(this.playListsService.api_all_playlists).subscribe(allPlaylists => {
-      this.playListsService.setPlayLists(allPlaylists);
-    });
-    
-    //! Fetch all sounds and their categories
-    this.http.get<any[]>(this.soundsService.apiGetAllSounds()).subscribe(sounds => {
-      this.soundsService.setSounds(sounds);
-      this.http.get<any[]>(this.soundsService.apiGetSoundsCategories()).subscribe(categories => {
-        this.soundsService.setCategories(categories);
-      });
-    });
-
-
-    //! Fetch all speeches 
-    this.http.get<any[]>(this.speechesService.api_get_all_speeches).subscribe(speeches => {
-      this.speechesService.setSpeeches(speeches);
-    });
-    
-  
   }
   
   checkIfUrlEndsWithApp() {
