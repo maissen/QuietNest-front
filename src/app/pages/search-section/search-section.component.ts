b517e291f8ffabd440fc8e527e82e957a483d9d3
Maissen Belgacem <maissen.developer500@gmail.com>
2025-01-19 12:49:23 +0100

    Initialize 'search-section' component for filtered speeches of a selected narrator.

--- src/app/pages/search-section/search-section.component.ts
+++ src/app/pages/search-section/search-section.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NarratorsService } from 'src/app/services/narrators.service';
 
 @Component({
   selector: 'app-search-section',
@@ -10,8 +12,18 @@ export class SearchSectionComponent implements OnInit {
 
   parameterType: string | null = null;
   parameterValue: string | null = null;
+  api_speeches_of_narrator = 'http://localhost:2003/api/speeches-of-narrator/';
+  api_speeches_oplaylists_by_category = 'http://localhost:2003/api/speeches-playlists-by-category/';
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  data: any;
+  narratorName: string = '';
+
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router,
+    private http: HttpClient,
+    public narrators: NarratorsService
+  ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -20,15 +32,29 @@ export class SearchSectionComponent implements OnInit {
 
         this.parameterType = 'narrator';
         this.parameterValue = params.get('narratorID');
+        this.http.get(this.api_speeches_of_narrator + this.parameterValue).subscribe(
+          (res: any) => {
+            this.data = res;
+          },
+          (err) => {
+            console.log(err);
+          }
+        );
       
-      } 
-      else if (this.router.url.includes('/category')) {
+      } else if (this.router.url.includes('/category')) {
       
         this.parameterType = 'category';
         this.parameterValue = params.get('categoryID');
+        this.http.get(this.api_speeches_oplaylists_by_category + this.parameterValue).subscribe(
+          (res: any) => {
+            this.data = res;
+          },
+          (err) => {
+            console.log(err);
+          }
+        );
       
-      } 
-      else if (this.router.url.includes('/time')) {
+      } else if (this.router.url.includes('/time')) {
       
         this.parameterType = 'time';
         this.parameterValue = params.get('timeID');
--- src/app/shared/narrator-profile/narrator-profile.component.ts
+++ src/app/shared/narrator-profile/narrator-profile.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { NarratorsService } from 'src/app/services/narrators.service';
 import { SpeechesService } from 'src/app/services/speeches.service';
@@ -12,28 +12,29 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class NarratorProfileComponent {
 
+  @Input() narrator: any;
+
   constructor(
     private user: UserService,
     public service: SpeechesService,
     private narratorsService: NarratorsService,
     private http: HttpClient,
-    private router: Router // Injecting the Router
+    private router: Router
   ) {}
 
   likeNarrator(): void {
-    let narrator = this.service.getSelectedSpeechData().narrator;
-    const body = { narratorID: narrator.id, userID: this.user.getUser().id };
+    const body = { narratorID: this.narrator.id, userID: this.user.getUser().id };
 
     this.http.post<any>(this.narratorsService.api_user_likes_narrator, body).subscribe({
       next: (response) => {
         console.log(response);
-        if (narrator.liked) {
-          narrator.likes = parseInt(narrator.likes) - 1;
+        if (this.narrator.liked) {
+          this.narrator.likes = parseInt(this.narrator.likes) - 1;
         } else {
-          narrator.likes = parseInt(narrator.likes) + 1;
+          this.narrator.likes = parseInt(this.narrator.likes) + 1;
         }
 
-        narrator.liked = !narrator.liked;
+        this.narrator.liked = !this.narrator.liked;
       },
       error: (err) => {
         console.error('Error:', err);
