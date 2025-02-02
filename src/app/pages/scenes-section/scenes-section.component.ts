1f03c7183e3d494d8bd2b0ced75c067b443c99f6
Maissen Belgacem <maissen.developer500@gmail.com>
2025-02-02 09:35:37 +0100

    Add loading animation when selecting a scene in 'scenes-section' component.

--- src/app/pages/scenes-section/scenes-section.component.ts
+++ src/app/pages/scenes-section/scenes-section.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ScenesSectionService } from 'src/app/services/scenes-section.service';
 import { ScenesService } from 'src/app/services/scenes.service';
+import { ToastService } from 'src/app/services/toast.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -11,13 +12,15 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class ScenesSectionComponent {
   scenes: any[] = [];
-  activeSceneId: number | null = null;
+  is_scene_loading: boolean = false;
+  target_scene_id: any = null;
 
   constructor(
     private router: Router,
     public service: ScenesSectionService,
-    private scenesService: ScenesService,
-    public user: UserService
+    public scenesService: ScenesService,
+    public user: UserService,
+    private toast: ToastService
   ) {}
 
   undoLink(): void {
@@ -25,15 +28,19 @@ export class ScenesSectionComponent {
   }
 
   updateUserScene(scene: any): void {
+    this.target_scene_id = scene.id;
     const userId = this.user.getUser().id;
+    this.is_scene_loading = true;
 
     this.user.updateUserScene(userId, scene.id).subscribe(
       (response) => {
         this.user.setUser(response);
         this.scenesService.setActiveScene(scene);
+        this.is_scene_loading = false;
       },
       (error) => {
         console.error('Error updating scene:', error);
+        this.toast.showToast('Failed to load scene', 2, 'try again');
       }
     );
   }
