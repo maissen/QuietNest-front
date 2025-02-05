b6b961c41f641e090e162051f8d341073dc38073
Maissen Belgacem <maissen.developer500@gmail.com>
2025-02-06 00:40:34 +0100

    Fix design of 'recommended-for-you-item'.

--- src/app/pages/create-section/components/audio-item/audio-item.component.ts
+++ src/app/pages/create-section/components/audio-item/audio-item.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { AppService } from 'src/app/services/app.service';
 import { CreateSectionSoundsService } from 'src/app/services/create-section-sounds.service';
 import { CreateSectionService } from 'src/app/services/create-section.service';
+import { SoundsService } from 'src/app/services/sounds.service';
 
 @Component({
   selector: 'app-audio-item',
@@ -16,7 +17,8 @@ export class AudioItemComponent {
   constructor(
     public service: CreateSectionService,
     public createSectionSoundsService: CreateSectionSoundsService,
-    public app: AppService
+    public app: AppService,
+    public soundsService: SoundsService
   ) {}
 
   updateSoundVolume(event: Event): void {
@@ -36,5 +38,12 @@ export class AudioItemComponent {
 
   toggleSound(sound: any): void {
     sound.isActive = !sound.isActive;
+
+    if(sound.isactive && this.soundsService.getActiveSounds().length == 1) {
+      if(this.soundsService.all_sounds_are_paused) {
+        this.soundsService.toggle_play_pause_all_sounds();
+      }
+      this.soundsService.desactivateAllSounds();
+    }
   }
 }
