7c79f632394a416ea0bbd448fd053be66b6b2ba0
Maissen Belgacem <maissen.developer500@gmail.com>
2025-02-09 15:02:13 +0100

    Update 'accordion's toggling animation' and make it smoother in 'welcome' page.

--- src/app/pages/welcome/components/welcome-page-accordion/welcome-page-accordion.component.ts
+++ src/app/pages/welcome/components/welcome-page-accordion/welcome-page-accordion.component.ts
@@ -1,15 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit, ViewChildren, QueryList, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-welcome-page-accordion',
   templateUrl: './welcome-page-accordion.component.html',
   styleUrls: ['./welcome-page-accordion.component.scss']
 })
-export class WelcomePageAccordionComponent {
+export class WelcomePageAccordionComponent implements AfterViewInit {
   @Input() items: { title: string; content: string }[] = [];
   openIndex: number | null = null;
 
+  @ViewChildren('accordionContent') accordionContents!: QueryList<ElementRef>;
+
   toggle(index: number) {
     this.openIndex = this.openIndex === index ? null : index;
+    this.updateHeights();
+  }
+
+  ngAfterViewInit() {
+    this.updateHeights();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.updateHeights();
+  }
+
+  private updateHeights() {
+    this.accordionContents.forEach((content, index) => {
+      const element = content.nativeElement;
+      if (this.openIndex === index) {
+        element.style.height = element.scrollHeight + 'px';
+      } else {
+        element.style.height = '0px';
+      }
+    });
   }
 }
