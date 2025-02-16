561c54eda107c313adcdaa849cf5775b0318cdde
Maissen Belgacem <maissen.developer500@gmail.com>
2025-02-16 11:04:41 +0100

    Disable 'scenes bg video sounds'.

--- src/app/shared/bg-video/bg-video.component.ts
+++ src/app/shared/bg-video/bg-video.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { ScenesService } from 'src/app/services/scenes.service';
-import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-bg-video',
   templateUrl: './bg-video.component.html',
   styleUrls: ['./bg-video.component.scss']
 })
-export class BgVideoComponent {
+export class BgVideoComponent implements AfterViewInit {
+  @ViewChild('videoPlayer') videoElement!: ElementRef<HTMLVideoElement>;
 
-  constructor(
-    public scenesService: ScenesService,
-    public user: UserService
-  ) {}
+  constructor(public scenesService: ScenesService) {}
 
+  ngAfterViewInit() {
+    if (this.videoElement) {
+      this.videoElement.nativeElement.muted = true;
+      this.videoElement.nativeElement.play().catch(err => {
+        console.warn('Autoplay prevented:', err);
+      });
+    }
+  }
 }
